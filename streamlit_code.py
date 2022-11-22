cd119dd0873a7171bc5568f9f7a2d16bcaa6afb1
Zhou <zhounan@1utar.my>
2022-11-22 17:30:37 +0800

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -17,6 +17,7 @@ test1="hg"
 test2="hut"
 sheet_url = st.secrets["public_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
+run_query(f'INSERT INTO "{sheet_url}" (name,pet) VALUES ("{test1}","{test2}")')
 
 
 # Print results.
