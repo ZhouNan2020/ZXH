10f5d2b2f69aae14e621c131785749f9dde25c01
Zhou <zhounan@1utar.my>
2022-11-24 07:59:06 +0800

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -170,7 +170,7 @@ with tab4:
                 a=plot_label(x,y1)[0]
                 b=plot_label(x,y1)[1]
                 for a, b in zip(a, b):
-                        plt.text(a + 0.1, b, b, ha='center', va='center', fontsize=14)
+                        ax1.text(a + 0.1, b, b, ha='center', va='center', fontsize=14)
                 st.pyplot(fig)
 
 
