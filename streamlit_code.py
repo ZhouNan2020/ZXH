9f9182203521223fdef3168d100eaa29e7a8e599
Zhou <zhounan@1utar.my>
2022-11-25 20:33:53 +0800

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -73,11 +73,11 @@ class today_count():
 
 with tab1:
         Breastfeeding = st.text_input('母乳亲喂（单位:分钟）', value='')
-        Breastfeeding = int(Breastfeeding)
+        Breastfeeding = int(float(Breastfeeding))
         BreastBottleFeeding = st.text_input('母乳瓶喂（单位:ml）', value='')
-        BreastBottleFeeding = int(BreastBottleFeeding)
+        BreastBottleFeeding = int(float(BreastBottleFeeding))
         FormulaMilkPowder = st.text_input('配方奶粉（单位:ml）', value='')
-        FormulaMilkPowder = int(FormulaMilkPowder)
+        FormulaMilkPowder = int(float(FormulaMilkPowder))
 
         Shit = st.checkbox('大便')
         Shit_value = 0
