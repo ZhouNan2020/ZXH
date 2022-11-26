e78647f9716400166b026d65b2b5bd49097ea2b8
Zhou <zhounan@1utar.my>
2022-11-26 21:22:14 +0800

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -133,7 +133,6 @@ class Analysis:
                     dataframe = pd.DataFrame(datafrmae.drop(self.datafrmae[self.datafrmae[str(name)] == 0].index))
                     dataframe = pd.concat([dataframe['date'], dataframe['ticks']], axis=1)
                     dataframe.set_index('date', inplace=True)
-
                     dataframe = dataframe['ticks'].diff(axis=0, periods=1)
                     st.write('1:', dataframe)
                     #dataframe.set_index('date', inplace=True)
