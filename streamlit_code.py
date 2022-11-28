c647d439b548b8b896a0be83615d2b5eae77573f
Zhou <zhounan@1utar.my>
2022-11-28 11:13:02 +0800

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -320,12 +320,12 @@ class suctionOfMilk:
         def lastSuckingTime(self):
                 data = self.datafrmae
                 st.write('data')
-                data = data[(pd.DataFrame(self.datafrmae)).columns[2]]
+                data = data['time']
                 data = (data.tail(1)).values[0]
                 return data
         def lastMilkML(self):
                 data = self.datafrmae
-                data = data[(pd.DataFrame(self.datafrmae)).columns[3]]
+                data = data['Quantity']
                 data = data.tail(1).values[0]
                 return data
         def dailyMilkIntake(self):
