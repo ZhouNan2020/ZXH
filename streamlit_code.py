3217afee7ccf498262d966137eca899e90168b91
Zhou <zhounan@1utar.my>
2022-12-09 16:23:11 +0800

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -130,7 +130,8 @@ class today_eatable:
         table_time = pd.to_datetime(table)
         time_diff = table_time.diff(periods=1)
         mean_time_diff = time_diff.mean()
-        mean_time_diff = mean_time_diff % 3600
+        mean_time_diff = mean_time_diff.total_seconds()
+        mean_time_diff = mean_time_diff//60
         return mean_time_diff
 
 
