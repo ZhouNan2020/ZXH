3bfba46776942cc51323a6938d46e2e25f0e8ef3
Zhou <zhounan@1utar.my>
2022-12-09 20:29:12 +0800

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -127,6 +127,7 @@ class today_eatable:
     def meanTimeInterval(self):
         table = pd.DataFrame(self.table)
         table = table.iloc[:, 2]
+        table = table.tail(20)
         table_time = pd.to_datetime(table)
         time_diff = table_time.diff(periods=1)
         mean_time_diff = time_diff.sum()
